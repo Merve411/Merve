ee5cea574d7fd8727a4840eab23505392dd49b77
Merve411 <acarnilgun997@gmail.com>
2026-07-19 17:11:58 +0300

    Use environment variable for token and fix reply text
    Replaced hardcoded token with environment variable and fixed string formatting in reply.

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
+import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = HTTP API:
-8622649861:AAEGLUxbWtJQeEborsxy5dBePONmpgBozmw
+TOKEN = os.environ["TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text( Merhaba )
+    await update.message.reply_text("Merhaba)
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
